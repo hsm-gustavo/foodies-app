986d67b9f6982f2c6b9e74fd23b4c5d96c0431d3
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2024-08-26 01:49:19 -0300

    fix: fixed css error caused by unhandled animations in tailwindcss

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <main className="not-found">
             <h1>Not found</h1>
-            <p>Sorry, this page doesn't exist!</p>
+            <p>Sorry, this page doesn&apos;t exist!</p>
         </main>
     )
 }
